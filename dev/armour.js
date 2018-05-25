31902dc0256ebb24d217e1b3b36e7250fe2c2d65
mrMinepPist <38004625+mrMinepPist@users.noreply.github.com>
2018-05-25 10:40:02 +0300

    Добавлены файлы в исходники

---
+++ dev/armour.js
@@ -0,0 +1,14 @@
+IDRegistry.genItemID("basicFrecuencemodule"); 
+Item.createItem("basicFrecuencemodule", "Basic frequency module", {name: "basicFrecuencemodule", meta: 1}, {maxDamage: 514});
+IDRegistry.genItemID("oxygenbigTank"); 
+Item.createItem("oxygenbigTank", "Big oxygen tank", {name: "oxygenbigTank", meta: 1}, {maxDamage: 2700});
+IDRegistry.genItemID("oxygenmedTank"); 
+Item.createItem("oxygenmedTank", "Medium oxygen tank", {name: "oxygenmedTank", meta: 1}, {maxDamage: 1800});
+IDRegistry.genItemID("oxygensmallTank"); 
+Item.createItem("oxygensmallTank", "Small oxygen tank", {name: "oxygensmallTank", meta: 1}, {maxDamage: 900});
+IDRegistry.genItemID("maskOxygen"); 
+Item.createItem("maskOxygen", "Oxygen mask", {name: "maskOxygen", meta: 1}, {maxDamage: 125});
+IDRegistry.genItemID("gearOxygen"); 
+Item.createItem("gearOxygen", "Oxygen gear", {name: "gearOxygen", meta: 1}, {maxDamage: 125});
+parachuteRegistry();
+
