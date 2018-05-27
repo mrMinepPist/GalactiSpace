d45c487510429b01d46257d97c031b4216a9e1e8
mrMinepPist <38004625+mrMinepPist@users.noreply.github.com>
2018-05-27 20:09:13 +0300

    Изменение файла

--- dev/core/materials.js
+++ dev/core/materials.js
@@ -1,5 +1,35 @@
-registryMaterial("Sapphire", "UNMETAL");
-registryMaterial("Silicon", "METAL");
+//регистрация материалов
+//лунные руды ванильных и индастриал материалов
+IDRegistry.genBlockID("oreTinm");
+Block.createBlock("oreTinm", [{name: Translation.translate("Moon Tin ore"), texture: [["oreTinm", 1]], inCreative: true}]);
+IDRegistry.genBlockID("oreCopperm");
+Block.createBlock("oreCopperm", [{name: Translation.translate("Moon Copper ore"), texture: [["oreCopperm", 1]], inCreative: true}]);
+//Сапфир лунный
+IDRegistry.genItemID("Sapphire");
+Item.createItem("Sapphire", Translation.translate("Sapphire"), {name: "Sapphire", meta: 1}, {});
+IDRegistry.genItemID("dustSapphire");
+Item.createItem("dustSapphire", Translation.translate("Sapphire dust"), {name: "dustSapphire", meta: 1}, {});
+IDRegistry.genItemID("plateSapphire");
+Item.createItem("plateSapphire", Translation.translate("Sapphire plate"), {name: "plateSapphire", meta: 1}, {});
+IDRegistry.genBlockID("oreSapphire");
+Block.createBlock("oreSapphire", [{name: Translation.translate("Moon sapphire ore"), texture: [["oreSapphire", 1]], inCreative: true}]);
+IDRegistry.genBlockID("blockSapphire");
+Block.createBlock("blockSapphire", [{name: Translation.translate("Sapphire block"), texture: [["blockSapphire", 1]], inCreative: true}]);
+//сыр(да, есть такое на луне, ну что поделаешь)
+IDRegistry.genBlockID("oreCheese");
+Block.createBlock("oreCheese", [{name: Translation.translate("Cheese ore"), texture: [["oreCheese", 1]], inCreative: true}]);
+//кремний
+IDRegistry.genItemID("ingotSilicon");
+Item.createItem("ingotSilicon", Translation.translate("Silicon ingot"), {name: "ingotSilicon", meta: 1}, {});
+IDRegistry.genItemID("dustSilicon");
+Item.createItem("dustSilicon", Translation.translate("Silicon dust"), {name: "dustSilicon", meta: 1}, {});
+IDRegistry.genItemID("plateSilicon");
+Item.createItem("plateSilicon", Translation.translate("Silicon plate"), {name: "plateSilicon", meta: 1}, {});
+IDRegistry.genBlockID("oreSilicon");
+Block.createBlock("oreSilicon", [{name: Translation.translate("Silicon ore"), texture: [["oreSilicon", 1]], inCreative: true}]);
+IDRegistry.genBlockID("blockSilicon");
+Block.createBlock("blockSilicon", [{name: Translation.translate("Silicon block"), texture: [["blockSilicon", 1]], inCreative: true}]);
+//жидкости
 IDRegistry.genItemID("oilBucket"); 
 Item.createItem("oilBucket", "Oil bucket", {name: "bucketOil", meta: 1}, {stack: 1});
 IDRegistry.genItemID("fuelBucket"); 
