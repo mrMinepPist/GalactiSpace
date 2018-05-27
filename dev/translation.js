b00fcbcf759977f43857590915c020423579f134
mrMinepPist <38004625+mrMinepPist@users.noreply.github.com>
2018-05-27 20:00:37 +0300

    Перевод

---
+++ dev/translation.js
@@ -0,0 +1,50 @@
+//Переводы
+//механизмы
+Translation.addTranslation("Refinery", {ru: "Центрифуга"});
+Translation.addTranslation("ROSCOSMOS workbench", {ru: "Ракетосборочный стол"});
+Translation.addTranslation("Oxygen comressor", {ru: "Кислородный компрессор"});
+Translation.addTranslation("Oxygen decomressor", {ru: "Кислородный декомпрессор"});
+Translation.addTranslation("Oxygen collector", {ru: "Кислородный коллектор"});
+Translation.addTranslation("Deconstructor", {ru: "Деконструктор"});
+//ракеты
+Translation.addTranslation("Rocket(tier 1)", {ru: "Ракета(1 уровень)"});
+//предметы
+//броня
+Translation.addTranslation("Basic frequency module", {ru: "Базовый радиочастотный модуль"});
+Translation.addTranslation("Big oxygen tank", {ru: "Большой кислородный баллон"});
+Translation.addTranslation("Medium oxygen tank", {ru: "Средний кислородный баллон"});
+Translation.addTranslation("Small oxygen tank", {ru: "Малый кислородный баллон"});
+Translation.addTranslation("Oxygen mask", {ru: "Шлем космонавта"});
+Translation.addTranslation("Oxygen gear", {ru: "Кислородная трубка космонавта"});
+//части ракеты
+Translation.addTranslation("Heavy duty plate", {ru: "Прочная обшивка"});
+Translation.addTranslation("Nose cone", {ru: "Головной обтекатель"});
+Translation.addTranslation("Rocket engine(Tier 1)", {ru: "Двигатель ракеты(1 уровень)"});
+Translation.addTranslation("Rocket fins", {ru: "Ступень ракеты"});
+//хранилища
+Translation.addTranslation("Copper canister", {ru: "Медная канистра"});
+Translation.addTranslation("Tin canister", {ru: "Оловянная канистра"});
+Translation.addTranslation("Air vent", {ru: "Воздуховод"});
+Translation.addTranslation("Air fan", {ru: "Вентилятор"});
+Translation.addTranslation("Oxygen concentrator", {ru: "Кислородный конденсатор"});
+Translation.addTranslation("Canvas", {ru: "Полотно"});
+Translation.addTranslation("Steel pole", {ru: "Шест от флага"});
+//трубы
+Translation.addTranslation("Oxygen pipe", {ru: "Кислородная трубка"});
+//материалы
+//руды из ванилы и индастриала
+Translation.addTranslation("Moon Tin ore", {ru: "Лунная оловянная руда"});
+Translation.addTranslation("Moon Copper ore", {ru: "Лунная медная руда"});
+//Сапфир
+Translation.addTranslation("Sapphire", {ru: "Сапфир"});
+Translation.addTranslation("Sapphire dust", {ru: "Сапфировая пыль"});
+Translation.addTranslation("Sapphire plate", {ru: "Сапфировая пластина"});
+Translation.addTranslation("Moon sapphire ore", {ru: "Лунная сапфировая руда"});
+Translation.addTranslation("Sapphire block", {ru: "Сапфировый блок"});
+//Кремний
+Translation.addTranslation("Silicon ingot", {ru: "Кремниевый слиток"});
+Translation.addTranslation("Silicon dust", {ru: "Кремниевая пыль"});
+Translation.addTranslation("Silicon plate", {ru: "Кремниевая пластина"});
+Translation.addTranslation("Silicon ore", {ru: "Кремниевая руда"});
+Translation.addTranslation("Silicon block", {ru: "Кремниевый блок"});
+Translation.addTranslation("Cheese ore", {ru: "Сырная руда"});
